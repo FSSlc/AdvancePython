2961e9a7af9bb7e7512b4b803d47118c773bcd1d
FSSlc <fsslc235@gmail.com>
2020-06-21 12:05:04 +0800

    利用多线程处理多个socket连接
    Signed-off-by: FSSlc <fsslc235@gmail.com>

--- ch10/socket_server.py
+++ ch10/socket_server.py
@@ -2,14 +2,28 @@
 # coding: utf-8
 
 import socket
+import threading
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('', 8000))
 server.listen()
-sock, addr = server.accept()
+
+
+def handle_sock(sock, addr):
+    # 获取从客户端获取的数据,一次获取 1K 的数据
+    while True:
+        data = sock.recv(1024)
+        print("addr = ", addr, "data = ", data.decode('utf8'))
+        re_data = input()
+        sock.send(re_data.encode('utf8'))
+    sock.close()
+
+
 # 获取从客户端获取的数据,一次获取 1K 的数据
-data = sock.recv(1024)
-print(data.decode('utf8'))
-sock.send('hello {}'.format(data.decode('utf8')).encode('utf8'))
-sock.close()
+while True:
+    sock, addr = server.accept()
+    # 用线程来处理新接收的连接
+    client_thread = threading.Thread(target=handle_sock, args=(sock, addr))
+    client_thread.start()
+
 server.close()
